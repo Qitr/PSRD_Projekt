182580805f25efc00e95c306adde9e153d00aeb3
Tamarix-gallica <62250937+Tamarix-gallica@users.noreply.github.com>
2020-05-27 00:35:26 +0200

    Wersja 27.05.2020
    W trakcie obliczeń depolaryzacji

--- skrypty/zaniki_wielodrogowe.m
+++ skrypty/zaniki_wielodrogowe.m
@@ -14,3 +14,18 @@ dG=10.5 - 5.6*log10(1.1-abs(cosd(2*52))^0.7)-2.7*log10(d)+1.7*log10(1+e);
 p=0.01;
 pw = p*10^(dG/10);
 A=-10*(log10(pw/((K*d^3.4).*((1+e).^(-1.03)).*f))+0.00076*h1)
+
+%% Obliczanie p-stwa depolaryzacji dla czystego powietrza
+XPDg = 32;
+if XPDg <= 35
+    XPD0 = XPDg+5;
+else
+    XPD0 = 40;
+end
+P0 = pw/100;
+eta = 1 - exp(-0.2*(P0)^0.75);
+k_XP = 0.7; % dla jednej anteny nadawczej
+Q = -10*log10(k_XP*eta/P0);
+C = XPD0 + Q
+
+
