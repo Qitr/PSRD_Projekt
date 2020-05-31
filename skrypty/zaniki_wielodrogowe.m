85f74a8335e3f9507ed6a9a0e9f0af8c81c1bd37
Tamarix-gallica <62250937+Tamarix-gallica@users.noreply.github.com>
2020-05-31 21:28:11 +0200

    Wersja 31.05.2020

--- skrypty/zaniki_wielodrogowe.m
+++ skrypty/zaniki_wielodrogowe.m
@@ -1,19 +1,22 @@
 close all;
 clear all;
 %% Obliczanie glebokosci zanikow wielodrogowych
-A = [20,20,20,2,2,2,2];%[dB]
-A= 20;
+% A = [20,20,20,2,2,2,2];%[dB]
+% A= 20;
 h1=37;h2 = 68.6323;
+hl = min(h1,h2);
 K = 1.7708e-05;
 eps =(h2-h1)/20.7;
-f = 25.571;psi = 52;
+f = [24.563 25.571]; %
+psi = 52;
 d=20.7;
-pw = (K*d^3.4).*((1+eps).^(-1.03)).*f.*10.^(-0.00076*h1-A/10);
-dG=10.5 - 5.6*log10(1.1-abs(cosd(2*52))^0.7)-2.7*log10(d)+1.7*log10(1+eps);
+% pw = (K*d^3.4).*((1+eps).^(-1.03)).*f.*10.^(-0.00076*h1-A/10);
+dG=10.5 - 5.6*log10(1.1-abs(cosd(2*psi))^0.7)-2.7*log10(d)+1.7*log10(1+eps)
 % p = (10^(-dG/10))*pw
 p=0.01;
-pw = p*10^(dG/10);
-A=-10*(log10(pw/((K*d^3.4).*((1+eps).^(-1.03)).*f))+0.00076*h1)
+pw = p*10^(dG/10)
+% A=-10*(log10(pw/((K*d^3.4).*((1+eps).^(-1.03)).*f))+0.00076*h1)
+A = -10*( log10(pw ./ (K*(d^3.4).* ((1+eps).^-1.03).*f)) + 0.00076*hl)
 
 
 %% Obliczanie p-stwa depolaryzacji dla czystego powietrza
